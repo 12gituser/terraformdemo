81dfabc0abdb26a28f3af109a4acd67972a8e260
Farooq <mfarooqmails@gmail.com>
2019-06-15 18:31:45 -0400

    moduleone.tf

--- moduleone.tf
+++ moduleone.tf
@@ -14,8 +14,8 @@ variable "key_name" {
 ##################################################################################
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = "${}"
+  secret_key = "${}"
   region     = "us-east-1"
 }
 
@@ -26,11 +26,11 @@ provider "aws" {
 resource "aws_instance" "nginx" {
   ami           = "ami-c58c1dd3"
   instance_type = "t2.micro"
-  key_name        = "${var.key_name}"
+  key_name        = "${}"
 
   connection {
     user        = "ec2-user"
-    private_key = "${file(var.private_key_path)}"
+    private_key = "${file()}"
   }
 
   provisioner "remote-exec" {
